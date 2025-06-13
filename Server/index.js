0956007aa33ca848a85855885a490d737e288bfc
Bhargav Patel <bhargavpatel1121@gmail.com>
2025-06-13 11:53:41 +0530

    add .txt file

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,34 @@
+// Making server using http modules
+
 const http = require("http");
+const fs = require("fs");
+const timeString = new Date().toLocaleTimeString();
 
+const port = 8081;
 const app = http.createServer((req, res) => {
-    // console.log(req.url)
-    // res.write("Welcome To My Server")
-    // res.end()
-    if (req.url == "/home"){
-        res.write("Welcome to home page")
-        res.end()
-    }else if(req.url == "/about"){
-        res.write("Welcomr to About Page")
-        res.end()
-    }else{
-        res.end("404 Page Not Found")
-    }
+    // console.log(req.url);   
+    // res.end();
+    if (req.url == "/") {
+        res.write("<h1>Hellow World</h1>");
+        fs.appendFileSync("log.txt", `\n ${timeString}  : Homepage`)
+        res.end();
 
+    } else if (req.url == "/contanctus") {
+        res.write("Prashant Sarviaya - 9316163578");
+        fs.appendFileSync("log.txt", `\n ${timeString}  : Contact Us`)
+        res.end();
+
+    }else if(req.url == "/favicon.ico") {
+
+    }
+    else {
+        res.write("404");
+        fs.appendFileSync("log.txt", `\n ${timeString}  : ${req.url}`)
+        res.end();
+
+    }
 });
 
-app.listen(8081, () => {
-    console.log("http://localhost:8081/")
-})
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
