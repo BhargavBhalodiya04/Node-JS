dca952c9d1948cd9c8bf0d69383824c18bc2f02c
Bhargav Patel <bhargavpatel1121@gmail.com>
2025-06-13 10:11:23 +0530

    Write A File, Read a file, Appending a file, Copying a file

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+// fs.writeFileSync("log.txt", "I Am Bhargav");
+
+// Writing A File
+// fs.writeFile("log.txt" , "This is Testing Data", (err ,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Data Writtedn Successfully")
+//     }
+// })
+
+// Reading A file sync
+// fs.readFileSync("log.txt" , "utf-8")
+
+// async
+// fs.readFile("log.txt" , "utf-8", (err,data) => {
+//     if(err) console.log(err)
+//         else console.log(data)
+// })
+
+// Appending in a file sync 
+// fs.appendFileSync("log.txt","\n My Name Is Bhargav Bhalodiya")
+
+// async
+// fs.appendFile("log.txt", "\n My Name Is Bhargav Bhalodiya", (err) => {
+//     if (err) {
+//         console.error("Error writing to file:", err);
+//     } else {
+//         console.log("Data appended successfully.");
+//     }
+// });
+
+// Copying a file
+// fs.cpSync("log.txt","Copyfile.txt")
+
+// removing a file
+// fs.unlinkSync("Copyfile.txt")
+// fs.cp("log.txt" , "newAsyncFile.txt",(err, data) =>{
+//     if(err) console.log(err)
+//         else console.log("Copy Successfull")
+// })
