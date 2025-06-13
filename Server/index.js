482c978051ec475263ac206c6ba9ddc2edfc734c
Bhargav Patel <bhargavpatel1121@gmail.com>
2025-06-13 11:14:30 +0530

    Server Start

---
+++ Server/index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+    // console.log(req.url)
+    // res.write("Welcome To My Server")
+    // res.end()
+    
+});
+
+app.listen(8081)
