acd8b9604fd37af5580989d5e7bbb62f8c29f1b6
Bhargav Patel <bhargavpatel1121@gmail.com>
2025-06-13 11:25:45 +0530

    server 404 error

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,18 @@ const app = http.createServer((req, res) => {
     // console.log(req.url)
     // res.write("Welcome To My Server")
     // res.end()
-    
+    if (req.url == "/home"){
+        res.write("Welcome to home page")
+        res.end()
+    }else if(req.url == "/about"){
+        res.write("Welcomr to About Page")
+        res.end()
+    }else{
+        res.end("404 Page Not Found")
+    }
+
 });
 
-app.listen(8081)
+app.listen(8081, () => {
+    console.log("http://localhost:8081/")
+})
