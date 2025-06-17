261e11274266258390788d1633c2eaadc07c2cac
Bhargav Patel <bhargavpatel1121@gmail.com>
2025-06-17 21:54:18 +0530

    add middleware

--- Express/index.js
+++ Express/index.js
@@ -7,57 +7,73 @@ const { type } = require("os");
 const app = express();
 const regFilePath = path.join(__dirname, "/login.html")
 
-app.use(express.urlencoded({extended: true}))
+// app.use(express.urlencoded({extended: true}))
 
-app.use(express.static(path.join(__dirname, "public")))
+// app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/" , (req, res) => {
     res.send("<h1 style='color:red;'>Welcome to the Homepage</h1>");
 } )
 
-app.get("/api/users" , (req,res) => {
-    res.json(data)
+// app.get("/api/users" , (req,res) => {
+//     res.json(data)
+// })
+
+// app.post("/api/users", (req, res) =>{
+//     const data = req.body
+//     const newData = {id: mockdata.length + 1, ...data}
+//     mockdata.push(newData);
+//     fs.wrtieFile("MOCK_DATA.json", JSON.stringify(mockdata),)
+//     if(err) console.log(err)
+//     res.json({success : true, data : req.body})
+// })
+
+// app.put("/api/users", (req, res) =>{
+//     res.json({success : true, type:"PUT"})
+// })
+
+// app.patch("/api/users", (req, res) =>{
+//     res.json({success : true, type:"PATCH"})
+// })
+
+// app.delete("/api/users", (req, res) =>{
+//     res.json({success : true, type:"DELETE"})
+// })
+
+// app.get("/api/users/:id" , (req,res) => {
+//     // console.log(req.params.id);
+//     const userid = parseInt(req.params.id)
+//     const showData = data.find((user) => {
+//         return user.id == userid
+//     })
+//     if(!showData){
+//         res.send("404")
+//     }
+//     res.json(showData)
+// })
+
+// // Serving a file
+// app.get("/login", (req,res) =>{
+//     res.sendFile(regFilePath)
+// })
+
+app.get("/" , (req, res) => {
+    res.send("<h1 style='color:red;'>Welcome to the Homepage</h1>");
+} )
+
+app.use((req, res, next) => {
+    console.log(req.url)
+    next();
 })
 
-app.post("/api/users", (req, res) =>{
-    const data = req.body
-    const newData = {id: mockdata.length + 1, ...data}
-    mockdata.push(newData);
-    fs.wrtieFile("MOCK_DATA.json", JSON.stringify(mockdata),)
-    if(err) console.log(err)
-    res.json({success : true, data : req.body})
+app.use((req, res, next) => {
+    console.log("Second middleware")
+    res.send("I got rejection")
 })
 
-app.put("/api/users", (req, res) =>{
-    res.json({success : true, type:"PUT"})
+app.get("/api/users", (req, res) => {
+    res.json({success : true, route : "/api/users"})
 })
-
-app.patch("/api/users", (req, res) =>{
-    res.json({success : true, type:"PATCH"})
-})
-
-app.delete("/api/users", (req, res) =>{
-    res.json({success : true, type:"DELETE"})
-})
-
-app.get("/api/users/:id" , (req,res) => {
-    // console.log(req.params.id);
-    const userid = parseInt(req.params.id)
-    const showData = data.find((user) => {
-        return user.id == userid
-    })
-    if(!showData){
-        res.send("404")
-    }
-    res.json(showData)
-})
-
-// Serving a file
-app.get("/login", (req,res) =>{
-    res.sendFile(regFilePath)
-})
-
-
 
 app.listen(port , () => {
     console.log(`http://localhost:${port}`);
