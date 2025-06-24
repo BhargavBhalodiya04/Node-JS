9c8ada4a30860e3b2b8df3399a808a57a81108e3
Bhargav Patel <bhargavpatel1121@gmail.com>
2025-06-24 10:49:02 +0530

    Display Username And Password

--- react/Use_State/src/App.jsx
+++ react/Use_State/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Use_State from './Use_State'
 import "./App.css"
 import Header from './props/Header'
 import Footer from './props/Footer'
+import Form from './props/Form'
 
 const App = () => {
   return (
     <>
       {/* <Use_State/> */}
-      <Header/>
-      <Footer/>
+      {/* <Header/> */}
+      {/* <Footer/>  */}
+      <Form/>
     </>
   )
 }
---
+++ react/Use_State/src/props/Form.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import '../App.css'; 
+
+const Form = () => {
+  const [name, setName] = useState('');
+  const [pass, setPass] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    console.log("User ID:", name);
+    console.log("Password:", pass);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        placeholder="Enter user ID"
+        className="input"
+        value={name}
+        name="name"
+      /><br />
+      <input
+        onChange={(e) => setPass(e.target.value)}
+        type="password"
+        placeholder="Enter user password"
+        className="input"
+        value={pass}
+        name="pass"
+      /><br />
+      <input
+        type="submit"
+        value="Login"
+        className="input"
+      />
+    </form>
+  );
+};
+
+export default Form;
