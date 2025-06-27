f00186fe7d19dd048b270fc43bbfab5d699de78e
Bhargav Patel <bhargavpatel1121@gmail.com>
2025-06-27 14:01:09 +0530

    code of module

--- react/Header/src/components/About.jsx
+++ react/Header/src/components/About.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const About = () => {
-  return <h2>About Us Page</h2>;
-};
+  const Navigate = useNavigate()
+  const handleSubmit = () => {
+    Navigate(-1)
+  }
+  return(
+    <> 
+    <h2>About Us Page</h2>
+    <button onClick={handleSubmit}>Home</button>
+    
+    </>
+    );
+    
+  };
 
 export default About;
